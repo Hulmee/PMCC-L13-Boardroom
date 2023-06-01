733dc8aff29ae8b9e7793be5e997ab0957fdfce4
Ellery <elleryh@insightsystems.com.au>
2023-06-01 16:12:11 +1000

    Changes to be committed: 	modified:   .gitignore 	modified:   package.json 	modified:   src/assets/_base.scss 	deleted:    src/style.css 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -18,4 +19,5 @@ export default defineConfig({
       },
     }),
   ],
+  base: './',
 })
