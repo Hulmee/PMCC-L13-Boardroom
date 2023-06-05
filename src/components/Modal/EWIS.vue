b7757fd1c398d5c71f7f43ba99a28e1a55109748
Ellery Hulme <elleryh@insightsystems.com.au>
2023-06-05 07:39:03 +0000

    Changes to be committed: 	modified:   src/components/Modal/EWIS.vue

---
+++ src/components/Modal/EWIS.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="modal">
+    <div class="modal-car container">
+      <h3>EMERGENCY SYSTEM ACTIVATED</h3>
+      <h1>FOLLOW INSTRUCTIONS FOR EVACUATION</h1>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  //
+</script>
+
+<style lang="scss" scoped>
+  @import '../../assets/colors';
+  .modal {
+    background: $fail;
+  }
+</style>
